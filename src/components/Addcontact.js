42cd2b159469dcea71f021795301f09ea953281f
Bhumi Y <121870630+ybhumi@users.noreply.github.com>
2023-07-17 01:01:53 +0530

    Add files via upload

---
+++ src/components/Addcontact.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Addcontact extends React.Component{
+    render(){
+        <div className="ui main">
+            <h2>Add COntact</h2>
+            <form className='ui form'>
+                <div className="field">
+                    <lable>Name</lable>
+                    <input type="text" name="name" placeholder="Name"/>
+                </div>
+                <div className="field">
+                    <lable>Name</lable>
+                    <input type="text" name="name" placeholder="Name"/>
+                </div>
+            </form>
+        </div>
+    }
+}
+export default Addcontact;
+
+
---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import './App.css';
+import Addcontact from "./Addcontact"; 
+import Header from "./Header";
+import Contactlist from "./Contactlist";
+
+
+function App() {
+  return (
+    <div >
+      <Header />
+      <Addcontact/>
+    {/* <Contactlist/> */}
+   
+    </div>
+  );
+}
+
+export default App;
